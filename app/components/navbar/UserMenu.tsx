cedd0cde4275bcec9faf0ffac5c94911ee87a740
joe95-ux <ogorktabi@gmail.com>
2023-09-17 23:30:58 +0100

    fix user menu errors

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,6 +4,7 @@ import Avatar from "../Avatar";
 import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
+import router from "next/router";
 
 
 const UserMenu = () => {
