fbd198254042ea70adc47556128bb326b09367fb
joe95-ux <ogorktabi@gmail.com>
2023-10-14 22:43:19 +0100

    add categories to navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -5,28 +5,30 @@ import Logo from './Logo';
 import Search from './Search';
 import UserMenu from './UserMenu';
 import { SafeUser } from '@/app/types';
+import Categories from './Categories';
 
-interface NavbarProps{
+interface NavbarProps {
   currentUser?: SafeUser | null;
 }
 
-const Navbar: React.FC<NavbarProps>= ({currentUser}) => {
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   return (
     <div className='fixed w-full bg-white z-10 shadow-sm'>
-        <div className='py-4 border-b-[1px]'>
-            <Container>
-                <div className="flex 
+      <div className='py-4 border-b-[1px]'>
+        <Container>
+          <div className="flex 
             flex-row 
             items-center 
             justify-between
             gap-3
             md:gap-0">
-                    <Logo/>
-                    <Search/>
-                    <UserMenu currentUser={currentUser}/>
-                </div>
-            </Container>
-        </div>
+            <Logo />
+            <Search />
+            <UserMenu currentUser={currentUser} />
+          </div>
+        </Container>
+      </div>
+      <Categories/>
     </div>
   )
 }
