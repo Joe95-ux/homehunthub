6673bb9d940dd45223f696ebc7fd151e99e8187f
joe95-ux <ogorktabi@gmail.com>
2023-10-16 22:17:11 +0100

    add category list

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -17,7 +17,7 @@ import { BsSnow } from 'react-icons/bs';
 import { IoDiamond } from 'react-icons/io5';
 import { MdOutlineVilla } from 'react-icons/md';
 
-// import CategoryBox from "../CategoryBox";
+import CategoryBox from "../CategoryBox";
 import Container from '../Container';
 
 export const categories = [
@@ -99,9 +99,38 @@ export const categories = [
 ]
 
 const Categories = () => {
+  const params = useSearchParams();
+  const category = params?.get('category');
+  const pathname = usePathname();
+  const isMainPage = pathname === '/';
+
+  if (!isMainPage) {
+    return null;
+  }
+
   return (
-    <div>Categories</div>
-  )
+    <Container>
+      <div
+        className="
+          pt-4
+          flex 
+          flex-row 
+          items-center 
+          justify-between
+          overflow-x-auto
+        "
+      >
+        {categories.map((item) => (
+          <CategoryBox 
+            key={item.label}
+            label={item.label}
+            icon={item.icon}
+            selected={category === item.label}
+          />
+        ))}
+      </div>
+    </Container>
+  );
 }
 
 export default Categories
